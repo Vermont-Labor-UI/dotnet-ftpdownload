db4140798f2281c5f331c3805d28ab6514b99374
Rocotton <Robert.Cotton@labor.idaho.gov>
2016-10-19 09:57:11 -0600

    Adding script

---
+++ ftp-download.ps1
@@ -0,0 +1,41 @@
+ # Config
+ $Username = $args[0]
+ $Password = $args[1]
+ $RemoteFile = $args[2]
+ $LocalFile = $args[3]
+
+ # Create a FTPWebRequest
+ $FTPRequest = [System.Net.FtpWebRequest]::Create($RemoteFile)
+ $FTPRequest.Credentials = New-Object System.Net.NetworkCredential($Username,$Password)
+ $FTPRequest.Method = [System.Net.WebRequestMethods+Ftp]::DownloadFile
+ $FTPRequest.UseBinary = $true
+ $FTPRequest.KeepAlive = $false
+ $FTPRequest.Timeout = 3000
+ $FTPRequest.EnableSsl = $true
+ Write-Host "Getting Response"
+ # Send the ftp request
+ $FTPResponse = $FTPRequest.GetResponse()
+
+if($FTPResponse -eq $null)
+{
+  Write-Host "FTP Response is null";
+  exit 1;
+}
+ Write-Host "Getting Response Stream"
+ # Get a download stream from the server response
+ $ResponseStream = $FTPResponse.GetResponseStream()
+
+ if($ResponseStream -eq $null)
+ {
+   Write-Host "Response Stream Is Null"
+   exit 1;
+ }
+ # Create the target file on the local system and the download buffer
+ $LocalFileFile = New-Object IO.FileStream ($LocalFile,[IO.FileMode]::Create)
+ [byte[]]$ReadBuffer = New-Object byte[] 1024
+ # Loop through the download
+ 	do {
+ 		$ReadLength = $ResponseStream.Read($ReadBuffer,0,1024)
+ 		$LocalFileFile.Write($ReadBuffer,0,$ReadLength)
+ 	}
+ 	while ($ReadLength -ne 0)
