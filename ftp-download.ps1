6671d4cec07849809e1529bec9fa52f644364e14
Alex Mitchell <alex@alappsis.com>
2017-02-28 15:38:15 -0700

    Changing writing of the stream to read to end,
    Old way could fail to get entire file if ftp stream is slow.

--- ftp-download.ps1
+++ ftp-download.ps1
@@ -33,12 +33,7 @@ if($FTPResponse -eq $null)
    Write-Host "Response Stream Is Null"
    exit 1;
  }
- # Create the target file on the local system and the download buffer
- $LocalFileFile = New-Object IO.FileStream ($LocalFile,[IO.FileMode]::Create)
- [byte[]]$ReadBuffer = New-Object byte[] 1024
- # Loop through the download
- 	do {
- 		$ReadLength = $ResponseStream.Read($ReadBuffer,0,1024)
- 		$LocalFileFile.Write($ReadBuffer,0,$ReadLength)
- 	}
- 	while ($ReadLength -ne 0)
+ #Write stream to local file
+ 
+ $reader = New-Object System.IO.StreamReader $ResponseStream
+ $reader.ReadToEnd() | Out-File $LocalFile
